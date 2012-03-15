361caec3a92f6cfb02c0b91826eaadbaea60d0fe
Dan Lash <danlash@gmail.com>
2012-03-15 16:13:55 -0400

    initial project

---
+++ HipChatConversations/Controllers/ExpressionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HipChatConversations.Controllers
+{
+    public class ExpressionController : Controller
+    {
+		[HttpPost]
+        public ActionResult Changed(string expression)
+		{
+
+			return new EmptyResult();
+		}
+
+    }
+}
