316fc318982c68b1e7575b6214e0b5d7880f4954
Pavel Mamut <pavel@versionone.com>
2012-04-25 19:23:59 -0400

    coming soon to Core :) - guess what that's useful for..

--- HipChatConversations/Models/MentionedAsset.cs
+++ HipChatConversations/Models/MentionedAsset.cs
@@ -2,6 +2,7 @@ namespace HipChatConversations.Models
 {
 	public class MentionedAsset
 	{
+		public string Id { get; set; }
 		public string Name { get; set; }
 		public string Number { get; set; }
 		public string AssetType { get; set; }
