07f9a6641a9212fc9833b807a33028d1bba517bb
Pavel Mamut <pavel@versionone.com>
2012-04-25 19:22:44 -0400

    removed various Member collections - they are used only for e-mail notifications and are not relevant to HipChat

--- HipChatConversations/Models/Expression.cs
+++ HipChatConversations/Models/Expression.cs
@@ -8,11 +8,7 @@ namespace HipChatConversations.Models
 		public string AuthorId { get; set; }
 		public string AuthorName { get; set; }
 		public string Content { get; set; }
-		public IEnumerable<Member> MentionedMembers { get; set; }
 		public IEnumerable<MentionedAsset> MentionedAssets { get; set; }
-		public IEnumerable<Member> ConversationParticipants { get; set; }
-		public IEnumerable<Member> MentionedAssetOwnersAndCustomers { get; set; }
 		public bool IsNewlyCreated { get; set; }
-		public IEnumerable<Member> MentionedAssetWatchers { get; set; }
 	}
 }
--- HipChatConversations/Models/Member.cs
+++
@@ -1,9 +0,0 @@
-namespace HipChatConversations.Models
-{
-	public class Member
-	{
-		public string Id { get; set; }
-		public string Name { get; set; }
-		public string Email { get; set; }
-	}
-}
